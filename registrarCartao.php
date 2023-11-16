f83c54132ba18b1f80a7dcf9275c8b48cec7f36b
Felipe Roberto Rocha <feliperrocha2005@gmail.com>
2023-11-16 12:21:27 -0300

    16/11 - 12:21

--- registrarCartao.php
+++ registrarCartao.php
@@ -16,26 +16,30 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $idCliente = $_POST['idCliente'];
-    $cartao = $_POST['cartao'];
+    if (isset($_POST['idCliente'])) {
+        $idCliente = $_POST['idCliente'];
+        $cartao = $_POST['cartao'];
 
-    // Aqui você pode realizar as validações necessárias antes de atualizar os dados no banco de dados
+        // Aqui você pode realizar as validações necessárias antes de atualizar os dados no banco de dados
 
-    // Verificar se o número do cartão já está cadastrado para outro cliente
-    $checkSql = "SELECT idCliente FROM clientes WHERE cartao = '$cartao' AND idCliente != $idCliente";
-    $checkResult = $conn->query($checkSql);
+        // Verificar se o número do cartão já está cadastrado para outro cliente
+        $checkSql = "SELECT idCliente FROM clientes WHERE cartao = '$cartao' AND idCliente != $idCliente";
+        $checkResult = $conn->query($checkSql);
 
-    if ($checkResult->num_rows > 0) {
-        echo "Erro: Este número de cartão já está cadastrado para outro cliente." . $conn->error . "<p><a href=\"visualizarUsuario.php\">Voltar</a></p>";
-    } else {
-        // Atualizar o campo de cartão de crédito na tabela de clientes
-        $updateSql = "UPDATE clientes SET cartao = '$cartao' WHERE idCliente = $idCliente";
-
-        if ($conn->query($updateSql) === TRUE) {
-            header("Location: menuAdm.php");
+        if ($checkResult->num_rows > 0) {
+            echo "Erro: Este número de cartão já está cadastrado para outro cliente." . $conn->error . "<p><a href=\"visualizarUsuario.php?id=" . $idCliente . "\">Voltar</a></p>";
         } else {
-            echo "Erro ao atualizar o cartão de crédito: " . $conn->error . "<p><a href=\"visualizarUsuario.php\">Voltar</a></p>";
+            // Atualizar o campo de cartão de crédito na tabela de clientes
+            $updateSql = "UPDATE clientes SET cartao = '$cartao' WHERE idCliente = $idCliente";
+
+            if ($conn->query($updateSql) === TRUE) {
+                header("Location: menuAdm.php");
+            } else {
+                echo "Erro ao atualizar o cartão de crédito: " . $conn->error . "<p><a href=\"visualizarUsuario.php\">Voltar</a></p>";
+            }
         }
+    } else {
+        echo "ID do cliente não fornecido.<p><a href=\"visualizarUsuario.php\">Voltar</a></p>";
     }
 }
 
